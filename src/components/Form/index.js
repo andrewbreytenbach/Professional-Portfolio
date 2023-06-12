7ec328a3209954b5a6a7a942b803cf2bb17a5c38
Andrew Breytenbach <117181767+andrewbreytenbach@users.noreply.github.com>
2023-06-12 13:46:57 -0400

    copied over boilerplate code from the module 20 classwork folder

---
+++ src/components/Form/index.js
@@ -0,0 +1,82 @@
+import React, { useState } from 'react';
+import './style.css';
+import { checkPassword, validateEmail } from '../../utils/helpers';
+
+function Form() {
+  const [email, setEmail] = useState('');
+  const [userName, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleInputChange = (e) => {
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    if (inputType === 'email') {
+      setEmail(inputValue);
+    } else if (inputType === 'userName') {
+      setUserName(inputValue);
+    } else if (inputType === 'password') {
+      setPassword(inputValue);
+    }
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    if (!validateEmail(email) || !userName) {
+      setErrorMessage('Email or username is invalid');
+      return;
+    }
+
+    if (!checkPassword(password)) {
+      setErrorMessage(`Choose a more secure password for the account: ${userName}`);
+      return;
+    }
+
+    setUserName('');
+    setPassword('');
+    setEmail('');
+    alert(`Hello ${userName}`);
+  };
+
+  return (
+    <div>
+      <p>Hello {userName}</p>
+      <form className="form">
+        <input
+          value={email}
+          name="email"
+          onChange={handleInputChange}
+          type="email"
+          placeholder="email"
+        />
+        <input
+          value={userName}
+          name="userName"
+          onChange={handleInputChange}
+          type="text"
+          placeholder="username"
+        />
+        <input
+          value={password}
+          name="password"
+          onChange={handleInputChange}
+          type="password"
+          placeholder="password"
+        />
+        <button type="button" onClick={handleFormSubmit}>
+          Submit
+        </button>
+      </form>
+      {errorMessage && (
+        <div>
+          <p className="error-text">{errorMessage}</p>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Form;
