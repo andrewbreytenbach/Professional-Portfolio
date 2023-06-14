758a8a5b38c4bf7c3359ce12ad5c7d9ac58dc24c
Andrew Breytenbach <117181767+andrewbreytenbach@users.noreply.github.com>
2023-06-14 15:01:37 -0400

    updated the files in the public folder

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ function About() {
       <div>
         <div className="row">
           <div className="col-lg-4" data-aos="fade-right">
-            <img src="img/about-me.JPG" className="img-fluid" alt="" />
+            <img src="../public/img/about-me.JPG" className="img-fluid" alt="" />
           </div>
           <div className="col-lg-8 pt-4 pt-lg-0 content" data-aos="fade-left">
             <h3>Full-Stack Web Developer</h3>
