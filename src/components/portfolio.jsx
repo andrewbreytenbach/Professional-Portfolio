0f1636c56cc5a41c12bd94643b8092acd104826c
Andrew Breytenbach <ambreyts15@gmail.com>
2023-06-15 08:57:33 -0400

    fixed the issue with the github links so now they redirect to the github page

--- src/components/portfolio.jsx
+++ src/components/portfolio.jsx
@@ -6,67 +6,56 @@ const Portfolio = () => {
     {
       imageSrc: './img/color-WeatherApp.png',
       imageAlt: 'Weather App',
-      imageLink: './img/color-WeatherApp.png',
       githubLink: 'https://github.com/andrewbreytenbach/weather-challenge',
     },
     {
       imageSrc: './img/work-day-scheduler.png',
       imageAlt: 'Work Day Scheduler',
-      imageLink: './img/work-day-scheduler.png',
       githubLink: 'https://github.com/andrewbreytenbach/work-day-scheduler',
     },
     {
       imageSrc: './img/Password.png',
       imageAlt: 'Password Generator',
-      imageLink: './img/Password.png',
       githubLink: 'https://github.com/andrewbreytenbach/Password-Generator',
     },
     {
       imageSrc: './img/color-quiz.png',
       imageAlt: 'JavaScript Quiz Game',
-      imageLink: './img/color-quiz.png',
       githubLink: 'https://github.com/andrewbreytenbach/coding-game-quiz',
     },
     {
       imageSrc: './img/Match.com.png',
       imageAlt: 'The Other Match.com',
-      imageLink: './img/Match.com.png',
       githubLink: 'https://github.com/andrewbreytenbach/movie-book-match',
     },
     {
       imageSrc: './img/RandomPlaylistGenerator.png',
       imageAlt: 'Random Playlist Generator',
-      imageLink: './img/RandomPlaylistGenerator.png',
       githubLink: 'https://github.com/andrewbreytenbach/random-playlist-generator',
     },
     {
       imageSrc: './img/ProfessionalReadmeGenerator.png',
       imageAlt: 'Professional Readme Generator',
-      imageLink: './img/ProfessionalReadmeGenerator.png',
       githubLink: 'https://github.com/andrewbreytenbach/professional-readme-generator',
     },
     {
       imageSrc: './img/SVGLogoMaker.png',
       imageAlt: 'SVG Logo Maker',
-      imageLink: './img/SVGLogoMaker.png',
       githubLink: 'https://github.com/andrewbreytenbach/svg-logo-maker',
     },
     {
       imageSrc: './img/SQLEmployeeTracker.png',
       imageAlt: 'Employee Tracker',
-      imageLink: './img/SQLEmployeeTracker.png',
       githubLink: 'https://github.com/andrewbreytenbach/sql-challenge-employee-tracker',
     },
     {
       imageSrc: './img/NoteTakerApp.png',
       imageAlt: 'Note Taker',
-      imageLink: './img/NoteTakerApp.png',
       githubLink: 'https://github.com/andrewbreytenbach/express-note-taker',
     },
   ];
 
   const [showLinks, setShowLinks] = useState(Array(portfolioItems.length).fill(false));
-
 
   const handleHover = (index, show) => {
     setShowLinks((prevState) => {
@@ -76,8 +65,6 @@ const Portfolio = () => {
     });
   };
 
-  
-  
   return (
     <section id="portfolio" className="portfolio section-bg">
       <div className="container">
@@ -97,12 +84,9 @@ const Portfolio = () => {
               >
                 <img src={item.imageSrc} className="card-img-top" alt={item.imageAlt} />
                 <div className={`portfolio-links ${showLinks[index] ? 'visible' : ''}`}>
-                  <a href={item.imageLink} className="portfolio-lightbox" title={item.imageAlt}>
-                    <i className="bx bx-plus"></i>
-                  </a>
-                  <a href={item.githubLink} title="More Details">
-                    <i className="bx bx-link"></i>
-                  </a>
+                  <p className="portfolio-link" onClick={() => window.open(item.githubLink, '_blank')}>
+                    GitHub Repository
+                  </p>
                 </div>
               </div>
             </div>
@@ -112,7 +96,5 @@ const Portfolio = () => {
     </section>
   );
 };
-
-
 
 export default Portfolio;
