a0385aeb68fdd148c380dcda84b2a2f9f083553a
Andrew Breytenbach <117181767+andrewbreytenbach@users.noreply.github.com>
2023-06-27 08:40:02 -0400

    working through heroku deployment issues

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navigation from './components/Navigation';
 import Footer from './components/Footer';
 import About from './components/About';
-import Portfolio from './components/Portfolio';
+import Portfolio from './components/portfolio';
 import Resume from './components/Resume';
 import Contact from './components/Contact';
 
@@ -17,7 +17,7 @@ class App extends Component {
         <Routes>
         <Route path="/" element={<About />} />
           <Route path="/About" element={<About />} />
-          <Route path="/Portfolio" element={<Portfolio />} />
+          <Route path="/portfolio" element={<Portfolio />} />
           <Route path="/Resume" element={<Resume />} />
           <Route path="/Contact" element={<Contact />} />
         </Routes>
